dda22d0eb72b02bd86ee29e9dec56b6ba0eeda58
Allen Zhang <mcuallen@gmail.com>
2020-06-16 09:32:06 +0800

    crud-m 05 mongo-db

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+const connectionString = 'mongodb://admin:password@127.0.0.1:27017/admin'
+// const connectionString = 'mongodb+srv://admin:password@127.0.0.1/starwars?retryWrites=true&w=majority&tls=false'
 
-
+MongoClient.connect(connectionString, {useUnifiedTopology:true},(err,client)=>{
+    if(err) return console.error(err)
+    console.log('connect to Database')
+    const db = client.db('star-wars-quotes')
+})
 app.listen(3000, function(){
     console.log('listening on 3000')
 })
