17b86d6acdfd73c392351c2aa364346c43c1617b
Allen Zhang <mcuallen@gmail.com>
2020-06-16 10:46:02 +0800

    crud-m 06 crud continued

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -5,21 +5,26 @@ const app = express();
 const connectionString = 'mongodb://admin:password@127.0.0.1:27017/admin'
 // const connectionString = 'mongodb+srv://admin:password@127.0.0.1/starwars?retryWrites=true&w=majority&tls=false'
 
-MongoClient.connect(connectionString, {useUnifiedTopology:true},(err,client)=>{
-    if(err) return console.error(err)
+MongoClient.connect(connectionString, {useUnifiedTopology:true}).then(client=>{
     console.log('connect to Database')
     const db = client.db('star-wars-quotes')
-})
-app.listen(3000, function(){
-    console.log('listening on 3000')
-})
+    const quotesCollection = db.collection('quotes')
 
-app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/',(req, res)=>{
-    res.sendFile(__dirname + '/index.html')
-})
+    app.get('/',(req, res)=>{
+        res.sendFile(__dirname + '/index.html')
+    })
 
-app.post('/quotes', (req, res)=>{
-    console.log(req.body)
-})
+    app.post('/quotes', (req, res)=>{
+        quotesCollection.insertOne(req.body).then(result =>{
+            res.redirect('/')
+            console.log('insert successfully')
+        })
+            .catch(error => console.error(error))
+    })
+    app.listen(3000, function(){
+        console.log('listening on 3000')
+    })
+}).catch(console.error)
+
