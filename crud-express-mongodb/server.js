0da967991a07b61e5c3b3d8eb63d4897ee2a4ae0
Allen Zhang <mcuallen@gmail.com>
2020-06-16 08:59:51 +0800

    crud-m 04 crud-create

--- crud-express-mongodb/server.js
+++ crud-express-mongodb/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -7,6 +8,12 @@ app.listen(3000, function(){
     console.log('listening on 3000')
 })
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res)=>{
+    console.log(req.body)
 })
